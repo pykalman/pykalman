f0e9bd2130c663b6048c9cbb2f9ac7c21f8416fa
Daniel Duckworth <duckworthd@gmail.com>
2012-08-05 23:28:52 -0700

    Added setuptools code

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'kalman',
+    version = '0.9',
+    author = 'Daniel Duckworth',
+    author_email = 'pykalman@gmail.com',
+    description = ('An implementation of the Kalman Filter, Kalman ' +
+      'Smoother, and EM algorithm in Python'),
+    license = 'BSD',
+    keywords = 'kalman filter smoothing em hmm tracking',
+    url = 'pykalman.github.com',
+    packages = find_packages(),
+    classifiers = [
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Science/Research',
+      'License :: OSI Approved :: BSD License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],
+    include_package_data = True,
+#    install_requires = [
+#      'numpy',
+#      'scipy',
+#      'Sphinx',
+#      'numpydoc',
+#    ],
+#    tests_require = [
+#      'nose',
+#    ]
+)
