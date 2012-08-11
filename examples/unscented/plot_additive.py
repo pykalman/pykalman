8c4d2f27ff18fe89e3b5969443f7933f1fd9b398
Daniel Duckworth <duckworthd@gmail.com>
2012-08-11 00:39:37 -0700

    Added basic examples for UnscentedKalmanFilter

---
+++ examples/unscented/plot_additive.py
@@ -0,0 +1,72 @@
+'''
+============================================
+General vs. Additive Unscented Kalman Filter
+============================================
+
+This example shows the difference between the general Unscented Kalman Filter
+and the Additive Unscented Kalman Filter.
+
+The general Unscented Kalman Filter (UKF) places no limitations on how noise
+interacts with the model. While this provides a wider range of potential
+models, it comes at the cost of additional computational burden. If your model
+contains additive noise, the Additive Unscented Kalman Filter (AddUKF) allows
+one to exploit that to reduce computational complexity. While results are not
+guaranteed to be the same for both methods, they are very similar.
+
+The figure drawn shows the true, hidden state; the state estimates given by the
+UKF; and finally the same given by the UKS.
+'''
+import numpy as np
+import pylab as pl
+from pykalman import AdditiveUnscentedKalmanFilter, UnscentedKalmanFilter
+
+# initialize parameters
+def transition_function(state, noise):
+    a = state[0] * np.sin(state[1]) + noise[0]
+    b = state[1] + noise[1]
+    return np.array([a, b])
+
+def observation_function(state, noise):
+    C = np.array([[-1, 0.5], [0.2, 0.1]])
+    return np.dot(C, state) + noise
+
+def additive_transition_function(state):
+    return transition_function(state, np.array([0, 0]))
+
+def additive_observation_function(state):
+    return observation_function(state, np.array([0, 0]))
+
+transition_covariance = np.eye(2)
+random_state = np.random.RandomState(0)
+observation_covariance = np.eye(2) + random_state.randn(2, 2) * 0.1
+initial_state_mean = [0, 0]
+initial_state_covariance = [[1, 0.1], [-0.1, 1]]
+
+# sample from model
+ukf = UnscentedKalmanFilter(
+    transition_function, observation_function,
+    transition_covariance, observation_covariance,
+    initial_state_mean, initial_state_covariance,
+    random_state=random_state
+)
+akf = AdditiveUnscentedKalmanFilter(
+    additive_transition_function, additive_observation_function,
+    transition_covariance, observation_covariance,
+    initial_state_mean, initial_state_covariance
+)
+states, observations = ukf.sample(50, initial_state_mean)
+
+# estimate state with filtering
+ukf_state_estimates = ukf.filter(observations)[0]
+akf_state_estimates = akf.filter(observations)[0]
+
+# draw estimates
+pl.figure()
+lines_true = pl.plot(states, color='b')
+lines_ukf = pl.plot(ukf_state_estimates, color='r')
+lines_akf = pl.plot(akf_state_estimates, color='g')
+pl.legend((lines_true[0], lines_ukf[0], lines_akf[0]),
+          ('true', 'UKF', 'AddUKF'),
+          loc='upper left'
+)
+pl.show()
---
+++ examples/unscented/plot_filter.py
@@ -0,0 +1,64 @@
+'''
+==============================================
+Using the Unscented Kalman Filter and Smoother
+==============================================
+
+This simple example shows how one may apply the Unscented Kalman Filter and
+Unscented Kalman Smoother to some randomly generated data.
+
+The Unscented Kalman Filter (UKF) and Rauch-Rung-Striebel type Unscented Kalman
+Smoother (UKS) are a generalization of the traditional Kalman Filter and
+Smoother to models with non-linear equations describing state transitions and
+observation emissions. Unlike the Extended Kalman Filter (EKF), which attempts
+to perform the same task by using the numerical derivative of the appropriate
+equations, the UKF selects a handful of "sigma points", passes them through the
+appropriate function, then finally re-estimates a normal distribution around
+those propagated points. Experiments have shown that the UKF and UKS are
+superior to the EKF and EKS in nearly all scenarios.
+
+The figure drawn shows the true, hidden state; the state estimates given by the
+UKF; and finally the same given by the UKS.
+'''
+import numpy as np
+import pylab as pl
+from pykalman import UnscentedKalmanFilter
+
+# initialize parameters
+def transition_function(state, noise):
+    a = np.sin(state[0]) + state[1] * noise[0]
+    b = state[1] + noise[1]
+    return np.array([a, b])
+
+def observation_function(state, noise):
+    C = np.array([[-1, 0.5], [0.2, 0.1]])
+    return np.dot(C, state) + noise
+
+transition_covariance = np.eye(2)
+random_state = np.random.RandomState(0)
+observation_covariance = np.eye(2) + random_state.randn(2, 2) * 0.1
+initial_state_mean = [0, 0]
+initial_state_covariance = [[1, 0.1], [-0.1, 1]]
+
+# sample from model
+kf = UnscentedKalmanFilter(
+    transition_function, observation_function,
+    transition_covariance, observation_covariance,
+    initial_state_mean, initial_state_covariance,
+    random_state=random_state
+)
+states, observations = kf.sample(50, initial_state_mean)
+
+# estimate state with filtering and smoothing
+filtered_state_estimates = kf.filter(observations)[0]
+smoothed_state_estimates = kf.smooth(observations)[0]
+
+# draw estimates
+pl.figure()
+lines_true = pl.plot(states, color='b')
+lines_filt = pl.plot(filtered_state_estimates, color='r')
+lines_smooth = pl.plot(smoothed_state_estimates, color='g')
+pl.legend((lines_true[0], lines_filt[0], lines_smooth[0]),
+          ('true', 'filt', 'smooth'),
+          loc='lower left'
+)
+pl.show()
