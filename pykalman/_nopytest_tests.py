dbf5b06c1bf71380ce075308613e121badb085f2
Franz Király <f.kiraly@ucl.ac.uk>
2025-02-01 00:05:56 +0100

    Update _nopytest_tests.py

--- pykalman/_nopytest_tests.py
+++ pykalman/_nopytest_tests.py
@@ -1,6 +1,6 @@
 """Tests to run without pytest, to check pytest isolation."""
 
-from skbase import all_objects
+from skbase.lookup import all_objects
 
 # all_objects crawls all modules excepting pytest test files
 # if it encounters an unisolated import, it will throw an exception
