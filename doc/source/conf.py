0f25f666d645ead1e15a5e2f60909bf9393db56d
Franz Király <f.kiraly@ucl.ac.uk>
2025-03-13 12:08:06 +0100

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,11 +27,11 @@ sys.path.insert(0, os.path.abspath('../../'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.todo',
-    'sphinx.ext.pngmath',
-    'numpydoc'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.todo",
+    "sphinx.ext.imgmath",
+    "numpydoc",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
