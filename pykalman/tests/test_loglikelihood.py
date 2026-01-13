46a6f1064eb16fef8ef0819e345c75006afd0900
Franz Király <fkiraly@gcos.ai>
2026-01-13 09:53:35 +0100

    gitignore

--- pykalman/tests/test_loglikelihood.py
+++ pykalman/tests/test_loglikelihood.py
@@ -16,7 +16,7 @@ def test_custom_parameter_inference():
         f = pk.KalmanFilter(
             transition_matrices=phi,
             transition_covariance=sigma,
-            observation_offsets=0.1 ** 2.0
+            observation_offsets=0.1**2.0
         )
 
         return -f.loglikelihood(y_)
@@ -24,18 +24,18 @@ def test_custom_parameter_inference():
     np.random.seed(0)
     f = pk.KalmanFilter(
         transition_matrices=0.98,
-        transition_covariance=0.05 ** 2.0,
-        observation_covariance=0.1 ** 2.0
+        transition_covariance=0.05**2.0,
+        observation_covariance=0.1**2.0
     )
 
     _, y = f.sample(500)
 
     y[[10, 25, 100], 0] = np.nan
-    x0 = np.array([0.5, np.log(0.1 ** 2.0)])
+    x0 = np.array([0.5, np.log(0.1**2.0)])
     res = minimize(fun, x0=x0, args=(y,), options={"maxiter": 1_000}, method="L-BFGS-B")
 
     assert res.success, "Optimization did not converge!"
-    
+
     phi_inferred = sigmoid(res.x[0])
 
     assert np.allclose(phi_inferred, f.transition_matrices, rtol=1e-1)
